d68bed2f23c7a6fab4611d3f9866893632bb2be5
olstart <olegstebenyev@gmail.com>
2023-09-04 15:19:53 +0300

    Initial commit

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-// Получаем модальное окно
-var modal = document.getElementById("myModal");
-
-// Получаем изображение и вставляем его в модальное окно
-var img = document.querySelectorAll('.portfolio-item img');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-
-img.forEach(function(image) {
-  image.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-  }
+$(document).ready(function() {
+    $('.portfolio-item').poptrox({
+        caption: function($a) {
+            return $a.find('img').attr('alt');
+        }
+    });
 });
-
-// Получаем элемент <span> для закрытия модального окна
-var span = document.getElementsByClassName("close")[0];
-
-// Когда пользователь нажимает на <span> (x), закрываем модальное окно
-span.onclick = function() { 
-  modal.style.display = "none";
-}
